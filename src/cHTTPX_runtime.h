e32ad2c354b2a0f4ffdb5a1649467d3b5db4a030
nenishyi <aworkprogramm@gmail.com>
2026-09-21 18:06:42 +0500

    feat: expose internal worker runtime stats

--- src/cHTTPX_runtime.h
+++ src/cHTTPX_runtime.h
@@ -2,10 +2,12 @@
 #define CHTTPX_RUNTIME_H
 
 #include "cHTTPX_serv.h"
+#include "cHTTPX_worker.h"
 
 int _chttpx_runtime_init(chttpx_serv_t* server);
 void _chttpx_runtime_listen(chttpx_serv_t* server);
 void _chttpx_runtime_request_stop(chttpx_serv_t* server);
 void _chttpx_runtime_cleanup(chttpx_serv_t* server);
+int _chttpx_runtime_worker_stats(chttpx_serv_t* server, chttpx_worker_stats_t* stats);
 
 #endif
